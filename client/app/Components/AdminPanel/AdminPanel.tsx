0ccf36918561a9a49828999f76a31f70fedaa6d6
soheila-entezar <166860318+soheila-entezar@users.noreply.github.com>
2024-11-19 06:58:48 -0800

    fix the css

--- client/app/Components/AdminPanel/AdminPanel.tsx
+++ client/app/Components/AdminPanel/AdminPanel.tsx
@@ -11,7 +11,7 @@ function AdminPanel() {
           <h1 className="w-[100%] h-[45%] font-[bold] font-semibold text-[22px]">Sign in to account</h1>
           <div className=" w-[100%] h-[45%] font-[thin] text-[15px] text-[#9ca3af]">Enter your email & password to login</div>
         </div>
-        <div className="w-[90%] sm:[80%] h-[70%] xl:h-[65%] md:h-[75%] flex flex-wrap items-center">
+        <form className="w-[90%] sm:[80%] h-[70%]  md:h-[65%] flex flex-wrap items-center">
           <div className="w-[100%] h-[20%]   m-auto">
             <label className="h-[20%] text-[14px]  font-semibold text-[#374151]">Email Address</label>
             <input
@@ -33,22 +33,22 @@ function AdminPanel() {
               </div>
             </div>
           </div>
-          <div className=" w-[100%] h-[30%] md:h-[25%]  flex flex-wrap justify-between  mt-4 xl:mt-4">
-            <div className="w-[100%] md:w-[50%] h-[23%] lg:h-[30%]">
+          <div className=" w-[100%] h-[30%] md:h-[25%]  flex flex-wrap justify-between  mt-4 md:mt-4">
+            <div className="w-[100%] md:w-[50%] h-[23%] md:h-[30%]">
                <input className="" type="checkbox" />
             <label>Remember password</label>
             </div>
            <a className="w-[100%] md:w-[40%] h-[23%]">Forgot password?</a>
-           <button  className="w-[100%] h-[35%] md:h-[47%] bg-[#006666] text-[#f8fafc] rounded-md font-light xl:mt-[15px]">sing in</button>
+           <button type="submit" className="w-[100%] h-[35%] md:h-[47%] bg-[#006666] text-[#f8fafc] rounded-md font-light sm:mt-[15px]">sing in</button>
           </div>
-          <div className=" w-[100%] h-[8%] md:h-[10%] lg:h-[15%] flex items-center"><h6 className="font-[bold] text-[#5b5e61]">Or Sign in with</h6></div>
+          <div className=" w-[100%] h-[8%] md:h-[10%]  flex items-center"><h6 className="font-[bold] text-[#5b5e61]">Or Sign in with</h6></div>
           <div className="w-[100%] flex flex-wrap justify-between lg:justify-between h-[12%]">
             <button className="w-[32%] md:w-[30%] h-[100%] flex bg-[#e5efef] hover:bg-[#f5f5f5]  rounded-md items-center justify-center px-2 my-2"><LuLinkedin  className="w-[20%] lg:w-[20%] text-[#0077b5] mx-1"/><p className="w-[48%] font-[thin]">linkedin</p> </button>
             <button className="w-[32%] md:w-[30%] h-[100%] flex bg-[#e5efef] hover:bg-[#f5f5f5]  rounded-md items-center justify-center  px-2 my-2"><TbBrandTwitter className="w-[20%] lg:w-[20%] text-[#0077b5] mx-1" /><p className="w-[48%] font-[thin]">twitter</p> </button>
             <button className="w-[32%] md:w-[30%] h-[100%] flex bg-[#e5efef] hover:bg-[#f5f5f5]  rounded-md items-center justify-center   my-2"><FiFacebook className="w-[20%] lg:w-[20%] text-[#0077b5] " /><p className="w-[55%] font-[thin]">facebook</p> </button>
           </div>
          
-        </div>
+        </form>
         <div className="h-[10%] md:h-[10%]  flex items-center">  <p className=" w-[100%] ">Don't have account? <Link href={''}>create account </Link></p></div>
       </div>
     </div>
